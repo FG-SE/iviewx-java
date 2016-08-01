1dba245ecba0b2784aa47125e9a73c8df7a078c6
Luca Fülbier <luca.fuelbier@t-online.de>
2016-08-01 19:48:58 +0000

    Added Connection test class.
    [ETIViewXConnectionTest]
    - No tests yet

---
+++ src/test/java/connection/ETIViewXConnectionTest.java
@@ -0,0 +1,16 @@
+package connection;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import connection.ETConnection;
+import connection.ETIViewXConnection;
+
+public class ETIViewXConnectionTest {
+	
+	@Test
+	public void temp() {
+		//ETIViewXConnection con1 = new ETIViewXConnection();
+	}
+	
+}
