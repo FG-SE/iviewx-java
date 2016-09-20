3c7aa906e8ddef846a0593babf1b6d2e84cdb79f
Luca Fülbier <luca.fuelbier@t-online.de>
2016-09-20 16:00:50 +0000

    Revisited the ETCalibrationManager documentation.

--- src/main/java/calibration/ETCalibrationManager.java
+++ src/main/java/calibration/ETCalibrationManager.java
@@ -1,9 +1,6 @@
 package calibration;
 
 /** Manages the calibration process for an eyetracker.
- *  <p>
- *  Currently only supports automatic calibration (opens a separate window).
- *  Custom calibration might get implemented in the future.
  * 
  *  @author Luca Fuelbier
  */
