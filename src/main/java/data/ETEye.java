8193a16883a10f608ccb7f18a4d5aa9b9a25d808
Luca Fülbier <luca.fuelbier@t-online.de>
2016-09-20 10:16:44 +0000

    Added a method to ETEye, which translates to a enum value from a byte.
    Note:
    - This is the actual commit, the previous commit only included the ETEye tests

--- src/main/java/data/ETEye.java
+++ src/main/java/data/ETEye.java
@@ -2,5 +2,20 @@ package data;
 
 public enum ETEye {
 	LEFT,
-	RIGHT
+	RIGHT;
+	
+	public static ETEye fromByte(byte b) throws IllegalArgumentException {
+		
+		char eyeCharacter = (char)(b & 0xFF);
+		
+		switch(eyeCharacter)
+		{
+			case 'l':
+				return LEFT;
+			case 'r':
+				return RIGHT;
+			default:
+				throw new IllegalArgumentException("Error: Could not convert byte to ETEye enum value. Invalid byte value.");
+		}
+	}
 }
