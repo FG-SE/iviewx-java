b20d23fa5fe66b3e2b0dc0907468762dca0ef74c
Luca Fülbier <luca.fuelbier@t-online.de>
2016-09-20 16:01:10 +0000

    Added documentation for the ETIViewXCalibrationManager.

--- src/main/java/calibration/ETIViewXCalibrationManager.java
+++ src/main/java/calibration/ETIViewXCalibrationManager.java
@@ -3,14 +3,35 @@ package calibration;
 import exception.ETErrorHandler;
 import iviewxapi.IViewXAPILibrary;
 
+/** Manages the calibration process for the RED-m eyetracker.
+ *  <p>
+ *  Calibration responsibility is delegated using the IView X SDK.
+ *  <p>
+ *  Currently only supports automatic calibration.
+ *  Custom calibration and advanced calibration settings might get added in the future.
+ * 
+ *  @author Luca Fuelbier
+ */
 public class ETIViewXCalibrationManager implements ETCalibrationManager {
 	
 	private IViewXAPILibrary iView;
 	
+	/** Constructs a CalibrationManager that uses the provided IView X SDK binding.
+	 * 
+	 *  @param lib IView X SDK binding for eyetracker communication
+	 */
 	public ETIViewXCalibrationManager(IViewXAPILibrary lib) {
 		iView = lib;
 	}
 	
+	/** Starts the automatic calibration.
+	 *  <p>
+	 *  A new window will open, which will present the user with points to fixate on.
+	 *  The user might be required to hit the Spacebar button when fixating the first point.
+	 *  <p>
+	 *  Manipulating the calibration settings is not supported yet, but might be added in 
+	 *  a future version.
+	 */
 	@Override
 	public void calibrate() {
 		int status = iView.iV_Calibrate();
