24a9007d6666c769028ab30a17fbab44eb9adecd
Luca Fülbier <luca.fuelbier@t-online.de>
2016-09-20 10:15:18 +0000

    Added a method to ETEye, which translates to a enum value from a byte.

---
+++ src/test/java/data/ETEyeTest.java
@@ -0,0 +1,32 @@
+package data;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+
+import data.ETEye;
+
+public class ETEyeTest {
+	
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void fromByte_correctByteSupplied_returnsCorrespondingEnumValue() {
+		byte l = 108;
+		byte r = 114;
+		
+		assertEquals(ETEye.LEFT, ETEye.fromByte(l));
+		assertEquals(ETEye.RIGHT, ETEye.fromByte(r));
+	}
+	
+	@Test
+	public void fromByte_incorrectByteSupplied_throwsException() {
+		byte a = 97;
+		
+		thrown.expect(IllegalArgumentException.class);
+		ETEye.fromByte(a);
+	}
+
+}
