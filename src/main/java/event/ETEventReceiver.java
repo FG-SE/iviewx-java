4ecb8f714af4542352fd332216d1b3165238101b
Luca Fülbier <luca.fuelbier@t-online.de>
2016-09-22 21:15:30 +0000

    Added documentation for ETEventReceiver.

--- src/main/java/event/ETEventReceiver.java
+++ src/main/java/event/ETEventReceiver.java
@@ -1,7 +1,15 @@
 package event;
 
+/** Receives eyetracking events from an eyetracker.
+ * 
+ *  @author Luca Fuelbier
+ */
 public interface ETEventReceiver {
 
+	/** Retrieves a single eyetracking event.
+	 * 
+	 *  @return Eyetracking event
+	 */
 	public ETEvent getEvent();
 	
 }
