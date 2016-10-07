282ce1dc5b89d10c25adabd19b7d54589717e99c
Luca Fülbier <luca.fuelbier@t-online.de>
2016-10-07 08:53:09 +0000

    Added documentation for ETValidationManager

--- src/main/java/validation/ETValidationManager.java
+++ src/main/java/validation/ETValidationManager.java
@@ -1,7 +1,12 @@
 package validation;
 
+/** Manages the validation process for an eyetracker.
+ * 
+ * @author Luca Fuelbier
+ */
 public interface ETValidationManager {
 	
+	/** Starts the automatic validation. */
 	public void validate();
 	
 }
