b5bd230c3d6462be565de774f90fd406dc59f173
Luca Fülbier <luca.fuelbier@t-online.de>
2016-10-07 07:11:29 +0000

    Added documentation for ETPlaybackSampleReceiver

--- src/main/java/sample/ETPlaybackSampleReceiver.java
+++ src/main/java/sample/ETPlaybackSampleReceiver.java
@@ -2,18 +2,50 @@ package sample;
 
 import java.util.Iterator;
 
-// TODO: Rename this class, using a name that better indicates the classes functionality
-
+/** Receives eyetracking samples from a ETSortedSampleList.
+ *  <p>
+ *  If you want to iterate the samples from the list again you will 
+ *  have to call the reset() method before requesting a new sample.
+ *  <p>
+ *  A sorted sample list is used to make sure, that samples are sorted 
+ *  beforehand to reduce computation effort while receiving samples.
+ * 
+ *  @author Luca Fuelbier
+ */
 public class ETPlaybackSampleReceiver implements ETSampleReceiver {
 	
 	ETSortedSampleList samples;
 	Iterator<ETSample> samplesIter;
 	
+	/** Constructs a new Playback SampleReceiver.
+	 *  <p>
+	 *  The sample list will initially be empty and has to be set manually.
+	 */
 	public ETPlaybackSampleReceiver() {
-		samples = new ETSortedSampleList();
-		reset();
+		this(new ETSortedSampleList());
+	}
+	
+	/** Constructs a nwe Playback SampleReceiver initializing it 
+	 *  with a provided ETSortedSampleList. 
+	 * 
+	 * @param samples Sample list
+	 */
+	public ETPlaybackSampleReceiver(ETSortedSampleList samples) {
+		this.samples = samples;
+		samplesIter = samples.iterator();
 	}
 
+	/** Receives a single eyetracking sample from the sample list.
+	 *  <p>
+	 *  The sample returned will be the next one in the list, or 
+	 *  null if the list is fully iterated or empty.
+	 *  <p>
+	 *  Note that this class does a full iteration of the sample list. 
+	 *  Timestamps do not matter. If you want a SampleReceiver that simulates 
+	 *  realtime sample access use ETTimedSampleReceiver instead. 
+	 * 
+	 *  @return Eyetracking sample
+	 */
 	@Override
 	public ETSample getSample() {
 		if(samplesIter.hasNext())
@@ -22,10 +54,19 @@ public class ETPlaybackSampleReceiver implements ETSampleReceiver {
 			return null;
 	}
 	
+	/** Resets the SampleReceiver to the beginning of the sample list.
+	 *  <p>
+	 *  Resetting essentially means, that you will iterate the sample list again. 
+	 *  This can be very helpful if you want to analyze the same set of data multiple times.
+	 */
 	public void reset() {
 		samplesIter = samples.iterator();
 	}
 	
+	/** Sets the sample list that will be used for sample retrieval.
+	 * 
+	 *  @param samples Sample list
+	 */
 	public void setSamples(ETSortedSampleList samples) {
 		this.samples = samples;
 		reset();
