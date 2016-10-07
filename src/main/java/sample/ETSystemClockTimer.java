765cf60ed457d9b5412635e7f577eeca1750f39c
Luca Fülbier <luca.fuelbier@t-online.de>
2016-10-07 08:12:06 +0000

    Added documentation for ETSystemClockTimer

--- src/main/java/sample/ETSystemClockTimer.java
+++ src/main/java/sample/ETSystemClockTimer.java
@@ -1,7 +1,15 @@
 package sample;
 
+/** Provides access to the current system time.
+ * 
+ * @author Luca Fuelbier
+ */
 public class ETSystemClockTimer implements ETTimer{
 
+	/** Returns the current system time in milliseconds.
+	 * 
+	 *  @Return Current system time [ms]
+	 */
 	@Override
 	public long getTime() {
 		return System.currentTimeMillis();
