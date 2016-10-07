6f6b60b2568546a0cd52633319e9f099f2d29af4
Luca Fülbier <luca.fuelbier@t-online.de>
2016-10-07 07:34:50 +0000

    Added documentation for ETSampleStabilizationStrategy

--- src/main/java/sample/ETSampleStabilizationStrategy.java
+++ src/main/java/sample/ETSampleStabilizationStrategy.java
@@ -1,7 +1,22 @@
 package sample;
 
+/** Strategy for stabilizing retrieved eyetracking samples.
+ *  <p>
+ *  This interface is used by the rest of the Eyetracking library as 
+ *  part of the Strategy pattern. If you want to define a eyetracking sample 
+ *  stabilization strategy, subclass this interface, implement your algorithm 
+ *  in a overriden version of the getStabilizedSample() method and pass your 
+ *  finished strategy to a SampleReceiver accepting the interface.
+ * 
+ *  @author Luca Fuelbier
+ */
 public interface ETSampleStabilizationStrategy {
 	
+	/** Returns a stabilized eyetracking sample, computed from the one provided.
+	 * 
+	 * @param sample Original eyetracking sample
+	 * @return Stabilized eyetracking sample
+	 */
 	public ETSample getStabilizedSample(ETSample sample);
 	
 }
