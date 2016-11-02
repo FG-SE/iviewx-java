fd170a2aec26b09da857cc436d942ac58af14151
Luca Fülbier <luca.fuelbier@t-online.de>
2016-11-02 01:19:14 +0000

    Added tests for new ETEye methods

--- src/test/java/data/ETEyeTest.java
+++ src/test/java/data/ETEyeTest.java
@@ -29,5 +29,22 @@ public class ETEyeTest {
 		thrown.expect(IllegalArgumentException.class);
 		ETEye.fromByte(a);
 	}
+	
+	@Test
+	public void fromChar_correctCharSupplied_returnsCorrespondingEnumValue() {
+		char l = 'l';
+		char r = 'r';
+		
+		assertEquals(ETEye.LEFT, ETEye.fromChar(l));
+		assertEquals(ETEye.RIGHT, ETEye.fromChar(r));
+	}
+	
+	@Test
+	public void fromChar_incorrectCharSupplied_throwsException() {
+		char a = 'a';
+		
+		thrown.expect(IllegalArgumentException.class);
+		ETEye.fromChar(a);
+	}
 
 }
