c79e0479b3004ebe4582c2f2941a6d23c63a49e9
Luca Fülbier <luca.fuelbier@t-online.de>
2017-03-23 23:49:06 +0000

    Added documentation for ETResponse and ETResponseType

--- src/main/java/generic/ETResponseType.java
+++ src/main/java/generic/ETResponseType.java
@@ -1,9 +1,18 @@
 package generic;
 
+/** Type of a {@link generic.ETResponse}.
+ * 
+ *  @author Luca Fuelbier
+ */
 public enum ETResponseType {
 	
+	/** New data is available */
 	NEW_DATA,
+	
+	/** No new data is available */
 	NO_NEW_DATA_AVAILABLE,
+	
+	/** The receivers source is depleted */
 	SOURCE_DEPLETED
 	
 }
