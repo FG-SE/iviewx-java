ef1c4004a7af20cc92909ed710a63cb165a78a64
Luca Fülbier <luca.fuelbier@t-online.de>
2017-03-23 23:49:37 +0000

    Added documentation for ETStabilizationStrategy and ETDefaultStabilizationStrategy

--- src/main/java/generic/ETDefaultStabilizationStrategy.java
+++ src/main/java/generic/ETDefaultStabilizationStrategy.java
@@ -1,7 +1,23 @@
 package generic;
 
+/** Implementation of the {@link generic.ETStabilizationStrategy} interface which simply passes
+ *  through the data.
+ *  <p>
+ *  This is used as the default strategy in the SMI eyetracking library.
+ *  It is essentially a no-op strategy that doesn't correct the retrieved data.
+ * 
+ *  @see generic.ETStabilizationStrategy
+ *  
+ *  @author Luca Fuelbier
+ */
 public class ETDefaultStabilizationStrategy<E> implements ETStabilizationStrategy<E> {
 
+	/** Simply passes through the data, not performing any stabilization.
+	 * 
+	 *  @param sample Original data
+	 *  
+	 *  @return Unmodified data
+	 */
 	@Override
 	public E stabilize(E element) {
 		return element;
--- src/main/java/generic/ETStabilizationStrategy.java
+++ src/main/java/generic/ETStabilizationStrategy.java
@@ -1,7 +1,24 @@
 package generic;
 
+/** Strategy for stabilizing retrieved data.
+ *  <p>
+ *  This interface is used by the rest of the SMI eyetracking library as 
+ *  part of the <em>strategy pattern</em>. If you want to define a 
+ *  stabilization strategy, subclass this interface, implement your algorithm 
+ *  in a overriden version of the {@link #stabilize(Object) stabilize} method
+ *  and pass your finished strategy to a {@link generic.ETReceiver} accepting
+ *  the interface.
+ * 
+ *  @author Luca Fuelbier
+ */
 public interface ETStabilizationStrategy<E> {
 	
+	/** Stabilizes the provided data and returns it.
+	 * 
+	 *  @param element Original data
+	 *  
+	 *  @return Stabilized data
+	 */
 	public E stabilize(E element);
 	
 }
